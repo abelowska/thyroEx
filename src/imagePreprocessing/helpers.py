471ff47468a0730d74fcd4903d63c77d44a2ec21
Michał Grabowski <mkg.grabowski@gmail.com>
2019-06-13 14:58:47 +0200

    File wih helpers functions for data pre-processing

--- src/imagePreprocessing/helpers.py
+++ src/imagePreprocessing/helpers.py
@@ -1,35 +1,31 @@
 import cv2
-import os
-from collections import Counter
-
-import matplotlib.pyplot as plt
 
 
-# crop to square
-from src.imagePreprocessing.tickBarsDetector import ImageResizerFactory
-
-
+# crop to square without black padding after rotation
 def crop_image(image):
     y_size, x_size, _ = image.shape
     dest_size = min(y_size, x_size)
-    margin_x = x_size - dest_size
-    margin_y = y_size - dest_size
+
+    # TODO automatically calculated add margin
+    margin_x = x_size - dest_size + 70
+    margin_y = y_size - dest_size + 70
 
     print('{}, {}'.format(margin_x, margin_y))
 
-    image = image[int(margin_y/2):y_size - int(margin_y/2), int(margin_x/2):x_size - int(margin_x/2)]
-
-    return image
+    return image[int(margin_y / 2):y_size - int(margin_y / 2), int(margin_x / 2):x_size - int(margin_x / 2)]
 
 
-image_resizer = ImageResizerFactory().columbia_images()
+def resize_image(image, size):
+    return cv2.resize(image, (size, size))
 
-path = ''
-files_list = os.listdir(path)  # returns list
 
-for file in files_list:
-    image = cv2.imread(path + file)
-    image = crop_image(image)
+def rotation_augmentation(image, max_angle):
+    y_size, x_size, _ = image.shape
+    angle_list = range(-max_angle, max_angle)
 
-    image_resizer.save_image(image, path + file)
+    print(angle_list)
 
+    matrix_list = [cv2.getRotationMatrix2D((x_size / 2, y_size / 2), angle, 1) for angle in angle_list]
+    images = [cv2.warpAffine(image.copy(), M, (x_size, y_size)) for M in matrix_list]
+
+    return images
